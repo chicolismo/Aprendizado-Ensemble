687f91b527bc1b8d843c74279143036d55611a9d
Carlos F. Pinheiro <carlosfpinheiro@gmail.com>
2018-08-30 20:13:00 -0300

    Iniciando projeto

---
+++ main.py
@@ -0,0 +1,31 @@
+from collections import namedtuple
+import sys
+import csv
+
+
+def read_data(name, filename):
+    """
+    Converte um arquivo CVS numa lista de tuplas com o nome 'name' fornecido
+    """
+    data = []
+    with open(filename) as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        # Os nomes das colunas serão atributos com letras minúsculas
+        fieldnames = map(lambda s: s.lower(), next(reader))
+        Kls = namedtuple(name, fieldnames)
+        for row in reader:
+            data.append(Kls(*row));
+    return data
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+    else:
+        raise Exception('Forneça o nome do arquivo CSV')
+
+
+    rows = read_data('Condition', filename)
+    for row in rows:
+        print(row[-1])
