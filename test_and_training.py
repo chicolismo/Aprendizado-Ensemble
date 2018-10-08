b923734ddaf4d3809b351968b3a4bb25dcaebf50
Carlos Francisco Pinheiro <carlosfpinheiro@gmail.com>
2018-10-08 00:55:28 -0300

    Acrescentando seed no random

--- test_and_training.py
+++ test_and_training.py
@@ -1,140 +1,142 @@
 import random
-from collections import namedtuple
-from collections import defaultdict
-import tree as tr
 import math
+import statistics
+import tree as tr
+from collections import namedtuple, defaultdict
 
-Bootstrap = namedtuple('Bootstrap', ['training', 'test'])
+Bootstrap = namedtuple('Bootsrap', ('training', 'test'))
 
-# TODO: é preciso verificar se os conjutos são diferentes?
+RANDOM = random.Random(123)
+
 def bootstrap(data, r=10):
-    """
-    Separa o conjunto D em r conjuntos de teste e treino com reposição
-    """
+    '''
+    Separa o conjunto 'data' em r conjuntos de teste e treino com reposição
+    '''
     bootstrap_sets = []
-    # Cria r bootstraps
     for _ in range(r):
         training_set = []
-        # Seleciona n instâncias aleatórias (com repetição) para o conjunto de treino
+
+        # Seleciona n instâncias aleatórias (com repetição)
+        # para o conjunto de treinamento
         for _ in range(len(data)):
-            training_set.append(random.choice(data))
-        test_set = [inst for inst in data if inst not in training_set]
-        bootstrap_sets.append(Bootstrap(training=training_set, test=test_set))
+            training_set.append(RANDOM.choice(data))
+
+        test_set = [row for row in data if row not in training_set]
+
+        bootstrap_sets.append(
+            Bootstrap(training=training_set, test=test_set))
+
     return bootstrap_sets
 
 
-def stratifiedKFold(data, k=10):
+def stratified_k_fold(data, k=10):
     '''
-    Separa o conjunto de dados em k partições que mantém a proporção de instâncias por classe em cada fold
+    Separa o conjunto de dados em k partições que mantém a proporção
+    de instâncias por classe em cada fold
     '''
-    instancesPerClass = defaultdict(list)
+    instances_per_class = defaultdict(list)
     for instance in data:
-        instancesPerClass[instance[-1]].append(instance)
-    amountPerClass = defaultdict(list)
-    for c in instancesPerClass:
-        # Guarda o número (inteiro) de instâncias por fold e o resto da divisão por k
-        amountPerClass[c] = [len(instancesPerClass[c])//k, len(instancesPerClass[c])%k]
-    folds = [[] for l in range(k)]
-    for f in range(k):
-        for c in instancesPerClass:
-            # Entrega um número igual de instâncias de cada classe para cada fold
-            for i in range(amountPerClass[c][0]):
-                folds[f].append(instancesPerClass[c].pop())
-            # E mais uma instância (dentre as que ficaram de fora da divisão inteira)
-            # aos "resto da divisão" primeiros folds
-            if amountPerClass[c][1] >= 1:
-                folds[f].append(instancesPerClass[c].pop())
-                amountPerClass[c][1] = amountPerClass[c][1] - 1
+        instances_per_class[instance[-1]].append(instance)
+
+    amount_per_class = {}
+    for class_name, instances in instances_per_class.items():
+        # Guarda o número (inteiro) de instâncias for fold
+        # e o resto da divisão por k
+        size = len(instances)
+        amount_per_class[class_name] = [size // k, size % k]
+
+    folds = [[] for _ in range(k)]
+    for fold in folds:
+        for class_name, instances in instances_per_class.items():
+            # Entrega um número igual de instâncias de cada class para cada
+            # fold
+            for _ in range(amount_per_class[class_name][0]):
+                fold.append(instances.pop())
+
+            # E mais uma instância (dentre as que ficaram de fora da
+            # divisão inteira)
+            if amount_per_class[class_name][1] >= 1:
+                fold.append(instances.pop())
+                amount_per_class[class_name][1] -= 1
     return folds
 
 
-def crossValidation(D, L, numeric_indices=None, k=10, r=10):
-    Data = namedtuple('Data', L)
-    folds = stratifiedKFold(D, k)
-    # print("Número de folds: ", len(folds))
-    numOfClasses = tr.count_classes(D)
-    # confusionMatrix = [[0 for x in range(numOfClasses)] for y in range(numOfClasses)]
-    confusionMatrix = nested_dict(numOfClasses)
-    classes = list(set([e[-1] for e in D]))
-    # for c in classes:
-    #     for c2 in classes:
-    #         confusionMatrix[c][c2] = 0
+def cross_validation(data, attributes, numeric_fields=None, k=10, r=10):
+    Data = namedtuple('Data', attributes)
+    folds = stratified_k_fold(data, k)
+    n_classes = tr.count_classes(data)
+    confusion_matrix = nested_dict(n_classes)
+    classes = list(set(row[-1] for row in data))
     fmeasures = []
     for current_fold in folds:
-        for c in classes:
+        for c1 in classes:
             for c2 in classes:
-                confusionMatrix[c][c2] = 0
+                confusion_matrix[c1][c2] = 0
         training_data = []
         for fold in folds:
             if fold != current_fold:
                 for item in fold:
                     training_data.append(item)
-
-        # print('Training', training_data)
-        trees = tr.random_forest(training_data, L, numeric_indices, r)
+        trees = tr.random_forest(training_data, attributes, numeric_fields, r)
 
         for element in current_fold:
             decision = tr.majority_voting(trees, Data(*element[0:-1]))
-            confusionMatrix[element[-1]][decision] += 1
-            print("DECISAO: " + decision)
-        tp, fp, fn = sum_tp_fp_fn(confusionMatrix)
-        fmeasures.append(f_measure(1,tp,fp,fn))
-    aritmMean = mean(fmeasures)
-    stdDeviation = std_deviation(fmeasures)
-    return aritmMean, stdDeviation
+            print("Decisão: ", decision)
+            confusion_matrix[element[-1]][decision] += 1
+        tp, fp, fn = sum_tp_fp_fn(confusion_matrix)
+        fmeasures.append(f_measure(1, tp, fp, fn))
+    return statistics.mean(fmeasures), statistics.stdev(fmeasures)
+
 
 def nested_dict(n):
-    if n == 1:
+    if n is 1:
         return defaultdict(int)
     else:
-        return defaultdict(lambda: nested_dict(n-1))
+        return defaultdict(lambda: nested_dict(n - 1))
 
-def sum_tp_fp_fn(confusionMatrix, c=None):
-    '''Retorna a soma dos verdadeiros positivos, falsos positivos e falsos negativos
-    c = classe a ter a soma retornada: se None, faz a soma de todas as classes'''
+
+def sum_tp_fp_fn(confusion_matrix, target_class=None):
+    '''
+    Retorna a soma dos verdadeirs positivos, falsos positivos, e falsos
+    negativos.
+
+    target_class = classe que se quer a soma. Se for None, faz a soma
+    de todas as classes.
+    '''
     tp = 0
     fp = 0
     fn = 0
-    if c != None:
-        tp = confusionMatrix[c][c]
-        for l in confusionMatrix:
-            if c != l:
-                fp += confusionMatrix[l][c]
-                fn += confusionMatrix[c][l]
-        return tp, fp, fn
+    if not target_class is None:
+        tp = confusion_matrix[target_class][target_class]
+        for class_name in confusion_matrix:
+            if target_class != class_name:
+                fp += confusion_matrix[class_name][target_name]
+                fn += confusion_matrix[target_name][class_name]
     else:
-        for l in confusionMatrix:
-            for l2 in confusionMatrix:
-                if l != l2:
-                    fp += confusionMatrix[l2][l]
-                    fn += confusionMatrix[l][l2]
+        for c1 in confusion_matrix:
+            for c2 in confusion_matrix:
+                if c1 != c2:
+                    fp += confusion_matrix[c2][c1]
+                    fn += confusion_matrix[c1][c2]
                 else:
-                    tp += confusionMatrix[l][l2]
-        return tp, fp, fn
+                    tp += confusion_matrix[c1][c2]
+    return tp, fp, fn
 
-def precision(truePositives, falsePositives):
-    return truePositives/(truePositives+falsePositives)
 
-def recall(truePositives, falseNegatives):
-    return truePositives/(truePositives+falseNegatives)
+def precision(tp, fp):
+    return tp / (tp + fp)
+
+
+def recall(tp, fn):
+    return tp / (tp + fn)
+
 
 def f_measure(beta, tp, fp, fn):
-    b2 = beta*beta
-    prec = precision(tp, fp)
-    rev = recall(tp, fn)
-    return (1 + (b2))*((prec*rev)/((b2*prec)+rev))
+    try:
+        b2 = beta**2
+        prec = precision(tp, fp)
+        rec = recall(tp, fp)
+        return (1 + b2) * ((prec * rec) / (b2 * prec + rec))
 
-def mean(list):
-    sum = 0
-    for e in list:
-        sum += e
-    return sum/len(list)
-
-def std_deviation(list):
-    m = mean(list)
-    sSum = 0
-    for e in list:
-        sSum += (e - m)*(e - m)
-    sSum = sSum/len(list)
-    return math.sqrt(sSum)
-
+    except ZeroDivisionError:
+        return 0
